f21f4347ca4dc7a67903c5c7d11b74c2bd72f752
jahedripon9 <jahedahmed3170@gmail.com>
2023-03-03 20:53:14 +0600

    updating

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+const admin = require("firebase-admin");
+
+
 
 
 // Midleware
@@ -12,8 +15,29 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.piqtj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+
+    }
+    next();
+}
 
 async function run() {
 
@@ -21,12 +45,23 @@ async function run() {
         await client.connect();
         const database = client.db('FoodWagon');
         const fooditemsCollection = database.collection('foodItems');
+        const usersCollection = database.collection('users');
 
         // GET API
         app.get('/fooditems', async (req, res) => {
             const cursor = fooditemsCollection.find({});
             const fooditems = await cursor.toArray();
             res.send(fooditems);
+        })
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
         })
         // GET SINGLE Item
         app.get('/fooditems/:id', async (req, res) => {
@@ -56,6 +91,23 @@ async function run() {
             const result = await fooditemsCollection.deleteOne(query);
             res.json(result);
 
+        })
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You do not have access to make Admin' })
+            }
         })
 
     }
